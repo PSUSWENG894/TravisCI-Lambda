c39145ee88b6ab5341be6b911d7eddecdcf96822
Alex Payne <jap91@psu.edu>
2018-05-24 07:54:13 -0500

    Add submodules to github

---
+++ target-app/front-end/src/components/BuildInformation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>This is a simple component for capturing build data.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buildNumber: String,
+    buildUsername: String,
+    buildTimeout: Number,
+    buildRuntimeVersions: String,
+    buildTestPhase: String,
+    buildCompilePhase: String,
+    buildDeployPhase: String,
+    buildWasPreviousDeploy: String,
+    buildWasBuildCanceled: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
