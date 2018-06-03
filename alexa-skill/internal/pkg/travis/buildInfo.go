09302106175ccc28e3a7abb80a5109e0937cabb4
georgemgeorge <georgemgeorge@gmail.com>
2018-06-02 20:09:39 -0500

    Adding travis package internal/pkg/travis
    Contains three files,
    buildInfo - Call TravisAPI for latest build information
    buildTarget - Call Travis API to build project
    utils - utility function used by buildInfo
    
    Currently just unmarshalling API JSON responses into structs for future use.

---
+++ alexa-skill/internal/pkg/travis/buildInfo.go
@@ -0,0 +1,127 @@
+package travis
+
+import (
+	"time"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"io/ioutil"
+)
+
+//TravisCI response body from the build info API call
+type BuildsResponse struct {
+	Type           string `json:"@type"`
+	Href           string `json:"@href"`
+	Representation string `json:"@representation"`
+	Pagination     struct {
+		Limit   int  `json:"limit"`
+		Offset  int  `json:"offset"`
+		Count   int  `json:"count"`
+		IsFirst bool `json:"is_first"`
+		IsLast  bool `json:"is_last"`
+		Next    struct {
+			Href   string `json:"@href"`
+			Offset int    `json:"offset"`
+			Limit  int    `json:"limit"`
+		} `json:"next"`
+		Prev  interface{} `json:"prev"`
+		First struct {
+			Href   string `json:"@href"`
+			Offset int    `json:"offset"`
+			Limit  int    `json:"limit"`
+		} `json:"first"`
+		Last struct {
+			Href   string `json:"@href"`
+			Offset int    `json:"offset"`
+			Limit  int    `json:"limit"`
+		} `json:"last"`
+	} `json:"@pagination"`
+	Builds []struct {
+		Type           string `json:"@type"`
+		Href           string `json:"@href"`
+		Representation string `json:"@representation"`
+		Permissions    struct {
+			Read    bool `json:"read"`
+			Cancel  bool `json:"cancel"`
+			Restart bool `json:"restart"`
+		} `json:"@permissions"`
+		ID                int         `json:"id"`
+		Number            string      `json:"number"`
+		State             string      `json:"state"`
+		Duration          int         `json:"duration"`
+		EventType         string      `json:"event_type"`
+		PreviousState     string      `json:"previous_state"`
+		PullRequestTitle  interface{} `json:"pull_request_title"`
+		PullRequestNumber interface{} `json:"pull_request_number"`
+		StartedAt         time.Time   `json:"started_at"`
+		FinishedAt        time.Time   `json:"finished_at"`
+		Private           bool        `json:"private"`
+		Repository        struct {
+			Type           string `json:"@type"`
+			Href           string `json:"@href"`
+			Representation string `json:"@representation"`
+			ID             int    `json:"id"`
+			Name           string `json:"name"`
+			Slug           string `json:"slug"`
+		} `json:"repository"`
+		Branch struct {
+			Type           string `json:"@type"`
+			Href           string `json:"@href"`
+			Representation string `json:"@representation"`
+			Name           string `json:"name"`
+		} `json:"branch"`
+		Tag    interface{} `json:"tag"`
+		Commit struct {
+			Type           string    `json:"@type"`
+			Representation string    `json:"@representation"`
+			ID             int       `json:"id"`
+			Sha            string    `json:"sha"`
+			Ref            string    `json:"ref"`
+			Message        string    `json:"message"`
+			CompareURL     string    `json:"compare_url"`
+			CommittedAt    time.Time `json:"committed_at"`
+		} `json:"commit"`
+		Jobs []struct {
+			Type           string `json:"@type"`
+			Href           string `json:"@href"`
+			Representation string `json:"@representation"`
+			ID             int    `json:"id"`
+		} `json:"jobs"`
+		Stages    []interface{} `json:"stages"`
+		CreatedBy struct {
+			Type           string `json:"@type"`
+			Href           string `json:"@href"`
+			Representation string `json:"@representation"`
+			ID             int    `json:"id"`
+			Login          string `json:"login"`
+		} `json:"created_by"`
+		UpdatedAt time.Time `json:"updated_at"`
+	} `json:"builds"`
+}
+
+func BuildsInfo(urlPrefix string, repoSlug string, limit int, apiToken string){
+	url := urlPrefix + "repo/"+ repoSlug+ "builds?limit=" + strconv.Itoa(limit)
+
+	response := ApiGet(url, apiToken)
+
+	body, _ := ioutil.ReadAll(response.Body)
+	var responseStruct BuildsResponse
+	err := json.Unmarshal(body, &responseStruct)
+
+	defer response.Body.Close()
+
+	if(err != nil) {
+		panic(err)
+	}
+
+	handleBuildInfoResponse(responseStruct)
+}
+
+func handleBuildInfoResponse(responseStruct BuildsResponse){
+	//This just prints one of the fields from the response to
+	//see if it unmarshalled properly
+	fmt.Println("Latest Build ID: " , responseStruct.Builds[0].ID)
+}
+
+
+
---
+++ alexa-skill/internal/pkg/travis/buildTarget.go
@@ -0,0 +1,121 @@
+package travis
+
+import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"net/http"
+	"bytes"
+)
+
+//TravisCI request body for the build API call
+type RequestBody struct {
+	Request struct {
+		Branch  string `json:"branch"`
+		Message string `json:"message"`
+		Config  struct {
+			Env struct {
+				Matrix []string `json:"matrix"`
+			} `json:"env"`
+		} `json:"config"`
+	} `json:"request"`
+}
+
+//TravisCI response body from the build API call
+type ResponseBody struct {
+	Type              string `json:"@type"`
+	RemainingRequests int    `json:"remaining_requests"`
+	Repository        struct {
+		Type           string `json:"@type"`
+		Href           string `json:"@href"`
+		Representation string `json:"@representation"`
+		ID             int    `json:"id"`
+		Name           string `json:"name"`
+		Slug           string `json:"slug"`
+	} `json:"repository"`
+	Request struct {
+		Repository struct {
+			ID        int    `json:"id"`
+			OwnerName string `json:"owner_name"`
+			Name      string `json:"name"`
+		} `json:"repository"`
+		User struct {
+			ID int `json:"id"`
+		} `json:"user"`
+		ID      int    `json:"id"`
+		Message string `json:"message"`
+		Branch  string `json:"branch"`
+		Config  struct {
+			Env struct {
+				Matrix []string `json:"matrix"`
+			} `json:"env"`
+		} `json:"config"`
+	} `json:"request"`
+	ResourceType string `json:"resource_type"`
+}
+
+//trigger a TravisCI build
+func Build(repoSlug string, buildBranch string , message string, apiToken string, envVars []string){
+	reqBody := RequestBody{}
+
+	//set the variables
+	reqBody.Request.Branch = buildBranch
+	reqBody.Request.Message = message
+	reqBody.Request.Config.Env.Matrix = envVars
+
+	jsonRequest, _ := json.Marshal(reqBody)
+
+	/* Example of the marshalled json
+ {
+    "request": {
+        "branch": "master",
+        "message": "Request to build from API with GO",
+        "config": {
+            "env": {
+                "matrix": [
+                    "UNITTESTS=ALL",
+                    "INTTESTS=NONE"
+                ]
+            }
+        }
+    }
+}
+	 */
+
+	//build the POST request
+	req, _ := http.NewRequest("POST","https://api.travis-ci.org/repo/" +repoSlug+ "requests" , bytes.NewReader(jsonRequest))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Travis-API-Version", "3")
+	req.Header.Set("Authorization", "token " +apiToken)
+
+	//execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	//handle errors
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	handleBuildResponse(resp)
+}
+
+func handleBuildResponse(response *http.Response) {
+	body, _ := ioutil.ReadAll(response.Body)
+
+	//unmarshall the response body and print the id to prove it worked
+	var responseBody ResponseBody
+	err := json.Unmarshal(body, &responseBody)
+
+	if(err != nil){
+		panic(err)
+	}
+
+	//This just prints one of the fields from the response to
+	//see if it unmarshalled properly
+	fmt.Println("ID: ", responseBody.Request.ID)
+}
+
