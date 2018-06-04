d2c170b1bd7fd65383f4df2dea8d8435938b7e98
mikercline <mikercline@gmail.com>
2018-06-03 20:48:54 -0400

    Changed to sum based on string values

--- mrc5163/sumation/sum.go
+++ mrc5163/sumation/sum.go
@@ -1,9 +1,26 @@
 package main
 
-func Sum(x int, y int) int {
-	return x + y
+func Sum(stati []string) int {
+	total := 0;
+	for index := 0; index < len(stati); index++ {
+		if stati[index] == "Failed" || stati[index] == "Error" {
+			total++
+		}
+	}
+	return total
 }
 
 func main() {
-	Sum(5, 5)
+	test := make([]string, 10)
+	test[0] = "Success"
+	test[1] = "Failed"
+	test[2] = "Error"
+	test[3] = "Ignore"
+	test[4] = "Skipped"
+	test[5] = "Failed"
+	test[6] = "Failed"
+	test[7] = "Skipped"
+	test[8] = "Error"
+	test[9] = "Success"
+	Sum(test)
 }
--- mrc5163/sumation/sum_test.go
+++ mrc5163/sumation/sum_test.go
@@ -3,28 +3,29 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
+	testSlice := []string{"Success","Failed","Error","Failed","Success","Success","Success"}
+	total := Sum(testSlice)
+	if total != 3 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
 
 func TestSumTable(t *testing.T) {
 	tables := []struct {
-		x int
-		y int
-		n int
+		test []string
+		total int
 	}{
-		{1, 1, 1},
-		{1, 2, 2},
-		{2, 2, 4},
-		{5, 2, 10},
+		{[]string{"Success","Failed"}, 1},
+		{[]string{"Success","Failed","Error"}, 2},
+		{[]string{"Success","Failed","Error","Failed","Success","Success","Success"}, 3},
+		{[]string{"Error","Failed","Failed","Failed","Failed","Error","Error","Error",
+			"Success","Skipped","Ignored","Error","Failed","Success"}, 10},
 	}
 
 	for _, table := range tables {
-		total := Sum(table.x, table.y)
-		if total != table.n {
-			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		total := Sum(table.test)
+		if total != table.total {
+			t.Errorf("Sum of (%v) was incorrect, got: %d, want: %d.", table.test, total, table.total)
 		}
 	}
 }
