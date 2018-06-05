a0876cb5a9fd7c5db23e298c36693bb46e14eb9d
Steven Ford <evensteven01@gmail.com>
2018-06-05 00:10:21 -0400

    Added an entry point/handler for Lambda and Alexa

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+    "log"
+)
+
+type MyEvent struct {
+	Name string `json: "name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+    log.Print("Handling event " + name.Name)
+    return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func saveCurrentUser(user string) string {
+	return "You asked to save current user: " + user
+}
+
+func main() {
+    fmt.Printf("Hello, world!\n")
+    lambda.Start(HandleRequest)
+}
